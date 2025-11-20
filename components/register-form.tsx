12566a7e24e474cee18d3503052951fe9a4b6df1
Gregory <gregory.laigneau@protonmail.com>
2025-11-21 00:24:13 +0700

    Replace <a> tags with Link

--- components/register-form.tsx
+++ components/register-form.tsx
@@ -14,6 +14,7 @@ import {
   FieldLabel,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import Link from 'next/link';
 
 export function RegisterForm({
   className,
@@ -59,7 +60,7 @@ export function RegisterForm({
               <Field>
                 <Button type="submit">Create Account</Button>
                 <FieldDescription className="text-center">
-                  Already have an account? <a href="#">Sign in</a>
+                  Already have an account? <Link href="#">Sign in</Link>
                 </FieldDescription>
               </Field>
             </FieldGroup>
@@ -67,8 +68,8 @@ export function RegisterForm({
         </CardContent>
       </Card>
       <FieldDescription className="px-6 text-center">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
+        By clicking continue, you agree to our <Link href="#">Terms of Service</Link>{" "}
+        and <Link href="#">Privacy Policy</Link>.
       </FieldDescription>
     </div>
   )
